a270d47f69c2f1774f8ee2235083e34962ca11d9
chanti <nirajvarma1928@gmail.com>
2026-02-22 23:58:01 +0530

    second-commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const[color,setColor]=useState("white");
+  const[color,setColour]=useState("white");
 
   return(
     <>
       <div style={{ backgroundColor: color, height: "100vh" }}>
-        <button onClick={() => setColor("red")}>RED</button>
-        <button onClick={() => setColor("green")}>GREEN</button>
-        <button onClick={() => setColor("blue")}>BLUE</button>
+        <button onClick={() => setColour("red")}>RED</button>
+        <button onClick={() => setColour("green")}>GREEN</button>
+        <button onClick={() => setColour("blue")}>BLUE</button>
       </div>
       
       
